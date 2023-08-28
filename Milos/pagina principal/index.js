2be959616096c95dcb04dba386336ac91b894cbc
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2023-08-28 17:12:54 -0300

    Add files via upload

--- Milos/pagina principal/index.js
+++ Milos/pagina principal/index.js
@@ -1,8 +1,13 @@
+// Obtenemos la referencia al menú y al submenú
 const menuIcon = document.getElementById('menu-icon');
 const submenu = document.getElementById('submenu');
 
-menuIcon.addEventListener('click', (event) => {
-    if (event.target === menuIcon) {
-        submenu.classList.toggle('show');
-    }
+// Agregamos un evento al pasar el cursor por encima del icono del menú
+menuIcon.addEventListener('mouseenter', () => {
+    submenu.style.display = 'block';
+});
+
+// Agregamos un evento para ocultar el submenú cuando el cursor sale del menú
+submenu.addEventListener('mouseleave', () => {
+    submenu.style.display = 'none';
 });
