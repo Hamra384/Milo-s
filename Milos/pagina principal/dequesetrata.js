80f429597e49c0fb81cf4131fec9ab3efe600308
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2023-09-07 15:47:50 -0300

    gfq
    ffq

--- Milos/pagina principal/dequesetrata.js
+++ Milos/pagina principal/dequesetrata.js
@@ -1,6 +1,13 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.querySelector('.sidebar');
+// Obtenemos la referencia al menú y al submenú
+const menuIcon = document.getElementById('menu-icon');
+const submenu = document.getElementById('submenu');
 
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
+// Agregamos un evento al pasar el cursor por encima del icono del menú
+menuIcon.addEventListener('mouseenter', () => {
+    submenu.style.display = 'block';
+});
+
+// Agregamos un evento para ocultar el submenú cuando el cursor sale del menú
+submenu.addEventListener('mouseleave', () => {
+    submenu.style.display = 'none';
 });
