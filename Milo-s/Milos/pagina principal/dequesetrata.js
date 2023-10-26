bfe63f52b601daff70c124bdb56b8199fe3cf92a
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2023-10-26 12:07:31 -0300

    jhajahj

--- Milo-s/Milos/pagina principal/dequesetrata.js
+++ Milo-s/Milos/pagina principal/dequesetrata.js
@@ -2,9 +2,24 @@
 const menuIcon = document.getElementById('menu-icon');
 const submenu = document.getElementById('submenu');
 
+// Variable para controlar si el menú está desplegado o no
+let isSubMenuVisible = false;
+
 // Agregamos un evento al pasar el cursor por encima del icono del menú
 menuIcon.addEventListener('mouseenter', () => {
+    isSubMenuVisible = true;
     submenu.style.display = 'block';
+});
+
+// Agregamos un evento al salir del icono del menú
+menuIcon.addEventListener('mouseleave', () => {
+    isSubMenuVisible = false;
+    setTimeout(() => {
+        // Ocultamos el submenú solo si el cursor no está sobre el submenú
+        if (!isSubMenuVisible) {
+            submenu.style.display = 'none';
+        }
+    }, 200); // Puedes ajustar el valor del retardo (en milisegundos) según tus necesidades
 });
 
 // Agregamos un evento para ocultar el submenú cuando el cursor sale del menú
