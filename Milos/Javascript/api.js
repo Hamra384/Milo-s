284321b8d2d9b690fe0a519145059db90b923bee
Hamra384 <115006241+Hamra384@users.noreply.github.com>
2023-11-10 13:26:06 -0300

    a
    d

--- Milos/Javascript/api.js
+++ Milos/Javascript/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const bcrypt = require("bcrypt");
 
 const funcs = require("./sql.js");
 
@@ -8,10 +9,10 @@ app.get("/represalia", function (req, res){
     let cuerpo = req.body;
 });
 
-app.get('/InsertarUsuario',async (req, res) => {
+app.post('/RegistrarUsuario',async (req, res) => {
     const {Nombre_Usuario, Contraseña, Email}= req.body;
 
-    let reg = funcs.InsertarUsuario(req.body.Nombre_Usuario)
+    let reg = funcs.RegistrarUsuario(req.body.Nombre_Usuario)
     .then((resultado) => {
         console.log(resultado);
         if (resultado instanceof Error){
@@ -24,9 +25,9 @@ app.get('/InsertarUsuario',async (req, res) => {
     });
 });
 
-app.get('/InsertarHijo',async (req, res) => {
+app.post('/RegistrarHijo',async (req, res) => {
     const {nombreHijo, generoHijo, edadHijo, idUsuario}= req.body;
-    let reg = funcs.InsertarHijo(req.body.Nombre_Usuario)
+    let reg = funcs.RegistrarHijo(req.body.Nombre_Usuario)
     .then((resultado) => {
         console.log(resultado);
         if (resultado instanceof Error){
@@ -37,6 +38,22 @@ app.get('/InsertarHijo',async (req, res) => {
         }
 
     });
+});
+app.post('/iniciarSesion', async (req, res) => {
+    const { Nombre_Usuario, Contraseña } = req.body;
+
+    try {
+        const resultado = await funcs.iniciarSesion(Nombre_Usuario, Contraseña);
+
+        if (resultado.loggedIn) {
+            res.status(200).json({ message: resultado.message });
+        } else {
+            res.status(401).json({ message: resultado.message });
+        }
+    } catch (error) {
+        console.error("Error en la ruta de inicio de sesión:", error);
+        res.status(500).json({ message: "Error en el servidor" });
+    }
 });
 
 app.listen(8080);
--- Milos/Javascript/sql.js
+++ Milos/Javascript/sql.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 "use strict"; // Esto es para ayudarte a exportar las funciones
 
 // Hay 2 formas de realizar una conexión: Pool y Connection
@@ -37,7 +38,7 @@ async function QueryIn(string, values) {
     }
 }
  
-async function InsertarUsuario(nombre, contraseña, email) {
+async function RegistrarUsuario(nombre, contraseña, email) {
     const consulta = "INSERT INTO datos_user (Nombre_Usuario, Email, Contraseña) VALUES (?, ?, ?)";
     const values = [nombre, contraseña, email];
     try {
@@ -47,7 +48,7 @@ async function InsertarUsuario(nombre, contraseña, email) {
         console.error("Error al insertar usuario:", error);
     }
 }
-async function InsertarHijo(nombreHijo, generoHijo, edadHijo, idUsuario) {
+async function RegistrarHijo(nombreHijo, generoHijo, edadHijo, idUsuario) {
     const consulta = "INSERT INTO datos_hijos (Nombre_Hijo, Genero_Hijo, Edad_Hijo) VALUES (?, ?, ?)";
     const values = [nombreHijo, generoHijo, edadHijo, idUsuario];
 
@@ -58,10 +59,37 @@ async function InsertarHijo(nombreHijo, generoHijo, edadHijo, idUsuario) {
         console.error("Error al insertar hijo:", error);
     }
 }
+async function iniciarSesion(nombreUsuario, contraseña) {
+    try {
+        // Consulta SQL para obtener el usuario con el nombre proporcionado
+        const query = "SELECT * FROM datos_user WHERE Nombre_Usuario = ?";
+        const [results] = await connection.execute(query, [nombreUsuario]);
+
+        if (results.length === 0) {
+            // El usuario no existe
+            return { loggedIn: false, message: "Usuario no encontrado" };
+        }
+
+        const usuario = results[0];
+
+        // Comparar la contraseña proporcionada con la almacenada (usando bcrypt)
+        const match = await bcrypt.compare(contraseña, usuario.Contraseña);
+
+        if (match) {
+            // Usuario autenticado
+            return { loggedIn: true, message: "Inicio de sesión exitoso" };
+        } else {
+            // Contraseña incorrecta
+            return { loggedIn: false, message: "Credenciales incorrectas" };
+        }
+    } catch (error) {
+        console.error("Error al iniciar sesión:", error);
+        throw error;
+    }
+}
 
 
-
-module.exports = {InsertarHijo, InsertarUsuario};
+module.exports = {RegistrarHijo, RegistrarUsuario, iniciarSesion};
 
 
 // boton iniciar sesion: nombre usuario y contraseña.
